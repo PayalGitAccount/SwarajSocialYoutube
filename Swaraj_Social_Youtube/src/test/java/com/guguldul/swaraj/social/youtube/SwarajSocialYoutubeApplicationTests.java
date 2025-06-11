6820888427954576e8a0f7e5f5dee405a4473d18
payalmohanty826@gmail.com <payalmohanty@gmail.com>
2025-06-11 21:02:50 +0530

    commit

---
+++ Swaraj_Social_Youtube/src/test/java/com/guguldul/swaraj/social/youtube/SwarajSocialYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guguldul.swaraj.social.youtube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwarajSocialYoutubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
