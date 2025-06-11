5723a51588d637f2e054260abe953040fe445ecd
payalmohanty826@gmail.com <payalmohanty@gmail.com>
2025-06-11 21:03:38 +0530

    commit

---
+++ Swaraj_Social_Youtube/src/main/java/com/guguldul/swaraj/social/youtube/SwarajSocialYoutubeApplication.java
@@ -0,0 +1,13 @@
+package com.guguldul.swaraj.social.youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwarajSocialYoutubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwarajSocialYoutubeApplication.class, args);
+	}
+
+}
